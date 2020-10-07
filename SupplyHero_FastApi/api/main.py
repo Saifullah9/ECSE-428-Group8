5ef239a3f039732b1dec70bb7883d3768fc38a39
Kevin Chuong <kevin.chuong@mail.mcgill.ca>
2020-10-06 20:28:32 -0400

    Fastapi base design setup

---
+++ SupplyHero_FastApi/api/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+
+@app.post("/upload")
+async def create_uploaded_file(file: UploadFile = File(...)):
+    supply_str = pytesseract.image_to_string(Image.open(file.file)).splitlines()
+    supply_str = [elem for elem in supply_str if elem]
+    print(supply_str)
+    return {"filename": file.filename,
+            "list": supply_str}
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/upload" enctype="multipart/form-data" method="post">
+<input name="file" type="file" required>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
