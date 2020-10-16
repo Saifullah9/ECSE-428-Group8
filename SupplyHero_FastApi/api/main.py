2719cc7c0ac7f2f56c63dc3161cb327273cfac8f
Oscar Décéus <oscarboy311@gmail.com>
2020-10-16 02:35:32 -0400

    Added storing school supplies + HTTP error handling

--- SupplyHero_FastApi/api/main.py
+++ SupplyHero_FastApi/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Depends
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_users.db import MongoDBUserDatabase
@@ -20,6 +20,9 @@ import pytesseract
 # Database for users
 mongo_db_users = MongoSession(collection='users')
 user_db = MongoDBUserDatabase(UserDB, mongo_db_users.collection)
+
+# Database for school supply list
+mongo_db_supplies = MongoSession(collection='school_supplies')
 
 # Backend FastAPI Server Declaration 
 app = FastAPI()
@@ -58,20 +61,26 @@ app.include_router(
 
 
 @app.post("/upload")
-async def create_uploaded_file(file: UploadFile = File(...)):
+async def create_uploaded_file(user: User=Depends(fastapi_users.get_current_active_user), file: UploadFile = File(...)):
+    # Retrieve school supplies
     if file.content_type == 'image/jpeg' or file.content_type == 'image/png':
         supply_str = pytesseract.image_to_string(Image.open(file.file)).splitlines()
     elif file.content_type == 'application/pdf': # Will need Poppler
         image_obj = convert_from_bytes(file.file.read())
         if len(image_obj) > 1:
-            return {'error': 'PDF contains more than 1 page.'}
+            raise HTTPException(status_code=400, detail="PDF contains more than 1 page.")
         supply_str = pytesseract.image_to_string(image_obj[0]).splitlines()
     else:
-        return {'error': 'File is not an image.'}
+        raise HTTPException(status_code=400, detail="File is not an image.")
     supply_str = [elem for elem in supply_str if elem]
-    print(supply_str)
-    return {"filename": file.filename,
-            "list": supply_str}
+
+    # Store data to database
+    supplies_data = {'email': user.email, 'filename': file.filename , 'school_supplies': supply_str}
+    supplies_data = mongo_db_supplies.insert_json(supplies_data)
+    if supplies_data:
+        return {"Message": "Success"}
+    else:
+        raise HTTPException(status_code=400, detail="Could not send data to database.")
 
 
 @app.get("/")
--- SupplyHero_FastApi/db/mongo.py
+++ SupplyHero_FastApi/db/mongo.py
@@ -3,13 +3,13 @@ import motor.motor_asyncio
 
 class MongoSession:
 
-    def __init__(self, database_name="supply-hero", collection=None):
+    def __init__(self, collection=None, database_name="supply-hero"):
         DATABASE_URL = 'mongodb+srv://admin_user:8w5B1e3hz4UcGEs5@supply-hero.isdcn.mongodb.net/supply-hero?retryWrites=true&w=majority'
         self.client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URL, uuidRepresentation="standard")
         self.db = self.client[database_name]
         self.collection = self.db[collection]
 
     def insert_json(self, document):
-        self.collection.insert_one(document)
+        return self.collection.insert_one(document)
 
 
