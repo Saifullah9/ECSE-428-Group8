ca5ce8ff4a6a05d983ffd79009165bf3836c2d2f
saif <saifullah.elsayed@mail.mcgil.ca>
2020-10-16 16:45:35 -0400

    changed the structure of the App.js files and created blank components

--- SupplyHero_React/frontend-app/src/App.js
+++ SupplyHero_React/frontend-app/src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.min.css"// installed and imported bootstrap -Saifullah
+import { BrowserRouter as Router, Route } from "react-router-dom"; // installed react routers to make it easier to add urls to components -Saifullah
+import Navbar from "./components/navbar.component";
+import SupplyList from "./components/supply-list.component";
+import Register from "./components/register.component";
+import Login from "./components/login.component";
+import Logout from "./components/logout.component";
+import Upload from "./components/upload.component";
 import ImageUpload from './ImageUpload/ImageUpload'
 import MenuNavbar from './MenuNavbar/Navbar'
 
@@ -13,13 +21,26 @@ function App() {
    *             that may not be required to add features.
    */
   return (
-    <div className="App">
-      {/* Sample taken from https://material-ui.com/components/app-bar/ 
-          Feel free to change to support what's needed */}
+    <Router>
+   {/* <div className="App">
+
+    
+       Sample taken from https://material-ui.com/components/app-bar/ 
+          Feel free to change to support what's needed 
+     
+      I currently removed the sections below to make it easier to route and develop the modules first then enhance in another sprint -Saifullah
       <MenuNavbar />
       <ImageUpload /> 
-
-    </div>
+      
+    </div>*/}
+    <Navbar />
+      <br/>
+      <Route path = "/" exact component={SupplyList} />
+      <Route path = "/register"  component={Register} />
+      <Route path = "/login"  component={Login} />
+      <Route path = "/logout"  component={Logout} />
+      <Route path = "/upload/:id"  component={Upload} />
+    </Router>
   );
 }
 
