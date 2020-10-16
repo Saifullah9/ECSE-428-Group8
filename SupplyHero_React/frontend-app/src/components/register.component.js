ad231481c79a4bf0b24f44ad96cad1dfc40fd884
saif <saifullah.elsayed@mail.mcgil.ca>
2020-10-16 18:21:02 -0400

    Updated the display of login and register

--- SupplyHero_React/frontend-app/src/components/register.component.js
+++ SupplyHero_React/frontend-app/src/components/register.component.js
@@ -1,27 +1,79 @@
 import React, { Component } from 'react';
 
 export default class Register extends Component{
+    constructor(props){
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangePassword(e){
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const user = {
+            username:this.state.username,
+            password:this.state.password,
+        }
+
+        console.log(user);
+
+        this.setState({
+            username:'',
+            password:'',
+        })
+        //window.location = '/' // reroutes to home page after submitting
+        
+    }
+
+
+
     render(){
         return(
             <div>
-              <p>Your are now on the Register component</p>
-              <form>
-                <h1>Register</h1>
-                <input
-                name="email"
-                type="text"
-                placeholder="Enter your email"
-                className="textbox"
-                />
-                <input
-                name="password"
-                type="password"
-                placeholder="Enter your password"
-                className="textbox"
-                />
-                <button className="btn" type="submit">
-                Register
-                </button>
+              <h3>Register</h3>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Username: </label>
+                    <input
+                        type="text"
+                        required
+                        value={this.state.username}
+                        placeholder="Enter your email"
+                        className="form-control"
+                        onChange={this.onChangeUsername}
+                    />
+                    <label>Password: </label>
+                    <input
+                        type="text"
+                        required
+                        value={this.state.password}
+                        placeholder="Enter your password"
+                        className="form-control"
+                        onChange={this.onChangePassword}
+                    />
+                    </div>
+                    <div className="form-group">
+                <input type="submit" value="Register" className="btn btn-primary" />
+                </div>
               </form>
             </div>
         )
