be5411803619b7301bbbdf8ecdb30e7dbc081210
Barry Chen <barrychenswags@Barrys-MacBook-Pro.local>
2020-10-16 17:31:31 -0400

    add login components

--- SupplyHero_React/frontend-app/src/components/login.component.js
+++ SupplyHero_React/frontend-app/src/components/login.component.js
@@ -5,6 +5,19 @@ export default class Login extends Component{
         return(
             <div>
                 <p>Your are now on the Login component</p>
+                <h1>Login</h1>
+                <input
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+                className="textbox"
+                />
+                <input
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                className="textbox"
+                />
             </div>
         )
     }
