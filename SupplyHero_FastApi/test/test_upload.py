572721265b730c519e186b96b9c6e515cb40f61b
Kevin Chuong <kevin.chuong@mail.mcgill.ca>
2020-10-18 16:41:03 -0400

    Unit tests for upload

---
+++ SupplyHero_FastApi/test/test_upload.py
@@ -0,0 +1,44 @@
+from fastapi.testclient import TestClient
+
+from api.main import app
+
+test_client = TestClient(app)
+
+
+def test_upload_valid_img():
+    image_file = open("features/test_files/morris_supply.png", "rb")
+    login_info = {'username': 'parent@hotmail.com', 'password': 'a!s@d#'}
+    file_obj = {'file': ("morris_supply.png", image_file,'image/png')}
+    login_response = test_client.post("/login", data=login_info)
+    headers = {"Authorization": "Bearer " + login_response.json()['access_token']}
+    response = test_client.post("/upload", files=file_obj, headers = headers)
+    assert response.status_code == 200
+    assert response.json()['Message'] == "Success"
+
+
+def test_upload_invalid_img():
+    image_file = open("features/test_files/test.txt", "rb")
+    login_info = {'username': 'parent@hotmail.com', 'password': 'a!s@d#'}
+    file_obj = {'file': ("test.txt", image_file,'test/plain')}
+    login_response = test_client.post("/login", data=login_info)
+    headers = {"Authorization": "Bearer " + login_response.json()['access_token']}
+    response = test_client.post("/upload", files=file_obj, headers = headers)
+    assert response.status_code == 400
+    assert response.json()['detail'] == "File is not an image."
+
+def test_upload_valid_pdf():
+    image_file = open("features/test_files/morris_supply.pdf", "rb")
+    login_info = {'username': 'parent@hotmail.com', 'password': 'a!s@d#'}
+    file_obj = {'file': ("morris_supply.pdf", image_file, 'application/pdf')}
+    login_response = test_client.post("/login", data=login_info)
+    headers = {"Authorization": "Bearer " + login_response.json()['access_token']}
+    response = test_client.post("/upload", files=file_obj, headers = headers)
+    assert response.status_code == 200
+    assert response.json()['Message'] == "Success"
+
+def test_upload_not_authorized():
+    image_file = open("features/test_files/morris_supply.png", "rb")
+    file_obj = {'file': ("morris_supply.png", image_file,'image/png')}
+    response = test_client.post("/upload", files=file_obj)
+    assert response.status_code == 401
+    assert response.json()['detail'] == "Unauthorized"
