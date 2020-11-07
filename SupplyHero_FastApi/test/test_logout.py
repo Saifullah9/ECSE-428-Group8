2c79ef7775b4cada291f534405db1947b40ae149
Alex Stedman <alexander.stedman@mail.mcgill.ca>
2020-11-06 19:25:25 -0800

    add test logout to see if it works

--- SupplyHero_FastApi/test/test_logout.py
+++ SupplyHero_FastApi/test/test_logout.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
+from db.mongo import MongoSessionRegular
+
 import random
+import json
 from api.main import app
 
 client = TestClient(app)
@@ -33,49 +36,30 @@ def test_logout_user():
     assert response.json() != {
         "detail": "REGISTER_USER_ALREADY_EXISTS"
     }
-    # assert response.status_code == 422
 
-    # login_response = client.post("/login", json={"id": response.json()['id'], "email": email, "password": password})
-    # # assert login was success
-    # assert login_response.json() != {
-    #     "detail": "LOGIN_BAD_CREDENTIALS"
-    # }
-    # verify user is currently active
     assert response.json()["is_active"] == True
-    logout_response = client.post("logout")
-    print(logout_response.json())
+    login_response = client.post("/login", data={"username": email, "password": password})
+    jwt_token = json.loads(login_response.content)
+    jwt_token = jwt_token["access_token"]
+    headers = {"Authorization": "Bearer " + jwt_token}
+
+    logout_response = client.post("/logout", headers=headers)
+    assert logout_response.json() == {"user_email": email, "logout_success": "true"}
+
+
     # logout_response
     print('\n\n')
     print(response.json())
     print('\n\n')
+    print(login_response.json())
+    print(logout_response.json())
     print(response.json()["id"])
     print(response.json()["is_active"])
-
+    # print(user["is_active"])
     # print(login_response.json())
-
     # print(login_response.json())
     print('done')
 
 
 
-def test_register_existing_user():
-    # dummy test user
-    email = "testingemail@preexisting.com"
-    password = "testingpassword"
-
-    # add user in case not existing
-    response = client.post("/register", json={
-        "email": email,
-        "password": password
-    })
-
-    # add the same user again
-    response = client.post("/register", json={
-        "email": email,
-        "password": password
-    })
-
-    assert response.status_code == 400
-    assert response.json() == {"detail": "REGISTER_USER_ALREADY_EXISTS"}
-
-    # TODO: remove the user from the database before ending this test
+# def test_logout_non-existing_user():
